40cbbd907cc8984dba52e7efd59714df7c585d87
rpire <rubenpire7@gmail.com>
2022-01-11 18:39:56 -0400

    Connect the React app with the Rails API

--- app/javascript/pages/Greet.js
+++ app/javascript/pages/Greet.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getGreetings } from "../redux/greetings/greetings";
 
 const Greet = () => {
+  const dispatch = useDispatch();
+
   const greeting = useSelector((state) => state.greeting);
-  console.log(greeting);
 
   return (
     <>
       <Link to="/">Go back home</Link>
       <p>Press the button to be greeted!</p>
-      <p>ID: {greeting.id}, {greeting.greeting}</p>
+      <button onClick={() => dispatch(getGreetings())}>Be greeted!</button>
+      <h1>{greeting.greeting}</h1>
     </>
   );
 }
